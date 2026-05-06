99671a2eede29f004d614f886adb35068a8e5383
Namul <namul@Namuls-MacBook-Air.local>
2026-05-06 20:35:02 +0600

    third commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en"
+      lang="en" suppressHydrationWarning={true}
       className={`${OutfitFont.className}  h-full antialiased`}
     >
       <body>
